68ede5550884b96279addaf8119cce9609534bab
Michael Efrain Espinoza de Leon <mich.esp_1989@hotmail.com>
2019-06-15 11:58:45 -0600

    Subiendo CRUD

--- app/controller/manejoCatalogo/manejoCatalogo.js
+++ app/controller/manejoCatalogo/manejoCatalogo.js
@@ -2,4 +2,39 @@ let moduloCatalogo = angular.module('parcialAngular');
 
 moduloCatalogo.controller('catalogoController', function(){
     let vm = this;
+
+    let defaults = ()=>{
+        loadItem();
+        loadCatalogo();
+    }
+
+    let loadItem = ()=>{
+        vm.item = {};
+    }
+    let loadCatalogo = ()=>{
+
+        vm.catalogo = JSON.parse(localStorage.getItem('catalogo'))||[];
+
+        if(!vm.cabezeraCatalogo){
+            vm.cabezeraCatalogo = ['Marca', 'Modelo', 'Precio', 'Memoria RAM', 'Almacenamiento', 'Bateria', 'Display', 'OS'];
+            localStorage.setItem('cabezeraCatalogo',JSON.stringify(vm.cabezeraCatalogo));
+        }
+    }
+
+    vm.addItem = ()=>{
+        vm.catalogo.push(vm.item);
+        saveCatalogo(vm.catalogo);
+        vm.clean();
+        console.log(vm.catalogo);
+    }
+
+    let saveCatalogo = (item)=>{
+        localStorage.setItem('catalogo', JSON.stringify(item));
+    }
+
+    vm.clean = ()=>{
+        vm.item = loadItem();
+    }
+
+    defaults();
 });
